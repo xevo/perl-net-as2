5e1851bd6017a7a8322e8662c07fb95483ddd082
Ken Prows <ken@xev.net>
2013-08-27 11:30:52 -0400

    debugging openssl

--- lib/Net/AS2.pm
+++ lib/Net/AS2.pm
@@ -398,7 +398,7 @@ sub decode_message
             $merged_headers .
             encode_base64($content);
         $is_content_raw = 0;
-
+        
         $content = eval { $self->{_smime_enc}->decrypt($content); };
         if (my $error = $@)
         {
@@ -425,11 +425,11 @@ sub decode_message
         }
         
         $content = _pkcs7_base64($content);
+        my $original_content = $content;
         $content = eval { $self->{_smime_sign}->check($content); };
         if (my $error = $@)
         {
-            
-            warn "[OPENSSL ERROR] Content failed signature check: $error\nvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv\n$content\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n";
+            warn "[OPENSSL ERROR] Content failed signature check: $error\nvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv\n$original_content\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n";
         }
 
         return Net::AS2::Message->create_error_message(@new_prefix,
@@ -788,10 +788,11 @@ sub _parse_mdn
         # OpenSSL (Crypt::SMIME) in Windows cannot handle binary content, 
         # convert signature part to base64
         $content = _pkcs7_base64($content);
+        my $original_content = $content;
         $content = eval { $self->{_smime_sign}->check($content); };
         if (my $error = $@)
         {
-            warn "[OPENSSL ERROR] Content failed signature check: $error\nvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv\n$content\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n";
+            warn "[OPENSSL ERROR] Content failed signature check: $error\nvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv\n$original_content\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n";
             return Net::AS2::MDN->create_unparsable_mdn('MDN signature failed verification: ' . $@);
         }
     } else {
